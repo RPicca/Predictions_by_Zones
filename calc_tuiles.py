506401f8d03da6445ec98bf736e37adacad7602f
Romain PICCA <rpicca@forsk.com>
2023-11-08 14:42:36 +0100

    ajout support multi-polygones

--- calc_tuiles.py
+++ calc_tuiles.py
@@ -1,4 +1,5 @@
 from shapely.geometry import Polygon
+from shapely.geometry import MultiPolygon
 import numpy as np
 from shapely.prepared import prep
 import geopandas as gpd
@@ -8,14 +9,25 @@ def read_geometry(geo_file):
     f=open(geo_file, "r")
     lines=f.readlines()
     f.close()
-    poly=[]
-    for pt in lines[2:]:
-        a=pt.split("\t")
-        poly.append((float(a[0]),float(a[1])))
-    return Polygon(poly)
+    #poly=[]
+    #for pt in lines[2:]:
+    #    a=pt.split("\t")
+    #    poly.append((float(a[0]),float(a[1])))
+    l=1 #numéro de ligne
+    polys=[]
+    for i in range(int(lines[0].split(" ")[1])):#nbr de 'Regions'
+        pol=[]
+        for pt in range(int(lines[l])): #Nombre de points indiqué dans le fichier
+            l+=1
+            a=lines[l].split("\t")
+            pol.append((float(a[0]),float(a[1])))
+        l+=1
+        polys.append([pol,[]])
+    return MultiPolygon(polys)
 
 def grid_bounds(geom, delta, resolution):
     minx, miny, maxx, maxy = geom.bounds
+    #wedging
     minx=minx//resolution*resolution
     maxx=np.ceil((maxx-minx)/delta)*delta+minx
     maxy=np.ceil(maxy/resolution)*resolution
@@ -38,11 +50,11 @@ def partition(geom, delta, resolution):
 
 resolution=50
 tile_size=1000
-poly=read_geometry("D:\\Documents\\validation\\Addin\\Predictions by Zones\\tuiles\\geo.txt")
-geom = poly
-grid = partition(geom, tile_size, resolution)
-
-fig, ax = plt.subplots(figsize=(15, 15))
-gpd.GeoSeries(grid).boundary.plot(ax=ax)
-gpd.GeoSeries([geom]).boundary.plot(ax=ax,color="red")
+polys=read_geometry("D:\\Documents\\validation\\Addin\\Predictions by Zones\\tuiles\\geo.txt").geoms
+for poly in polys:
+    geom = poly
+    grid = partition(geom, tile_size, resolution)
+    fig, ax = plt.subplots(figsize=(15, 15))
+    gpd.GeoSeries(grid).boundary.plot(ax=ax)
+    gpd.GeoSeries([geom]).boundary.plot(ax=ax,color="red")
 plt.show()
